c6b8e1647fc25dce9924c701ab6e586c5853dbe1
amna-g <64984546+amna-g@users.noreply.github.com>
2021-05-30 02:28:40 -0700

    add main.dart
    add main file

--- lib/main.dart
+++ lib/main.dart
@@ -10,9 +10,50 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home:Scaffold(
-        body:Text("afia, amna.asifmicrobiologist30@gmail.com")
-      )
+      home: Scaffold(
+        backgroundColor: Color(0XFFF48FB1),
+        appBar: AppBar(
+          title: Center
+          
+          (child: Text("Login page")),) ,
+          body: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+              SizedBox(height: 50,),
+              Container(
+                width: 200,
+                height: 100,
+                child: TextField(
+                 
+  decoration: InputDecoration(
+    border: OutlineInputBorder(),
+    hintText: 'Enter a search term'
+  ),
+
+
+                )),
+               SizedBox(height: 50,),
+              Container(
+                width: 200,
+                height: 100,
+                child: TextField(
+ decoration: InputDecoration(
+    border: OutlineInputBorder(),
+    hintText: 'Enter a search term'
+  ),
+
+                )),
+
+
+                ElevatedButton(onPressed: (){}, child: Text("LOGIN"))
+
+            ],),
+          ),
+          
+          
+          ),
+      
     );
   }
 }
